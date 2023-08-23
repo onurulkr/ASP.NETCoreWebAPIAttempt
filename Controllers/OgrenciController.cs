e558f5434e970a8aee907172bf5c1679580ec813
onuru <onuru@DESKTOP-NC2A1UC>
2023-08-23 20:56:58 +0300

    Add project files.

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,37 @@
+﻿using ASP.NETCoreWebAPIAttempt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreWebAPIAttempt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OgrenciController : ControllerBase
+    {
+        static List<Ogrenci> ogrenciler = new List<Ogrenci>
+        {
+            new Ogrenci{ Id = 1, AdSoyad = "Onur Ülker" },
+            new Ogrenci{ Id = 2, AdSoyad = "Can Gezgin" },
+            new Ogrenci{ Id = 2, AdSoyad = "Şafak Yaman" }
+        };
+
+        [HttpGet]
+        public List<Ogrenci> Get()
+        {
+            return ogrenciler;
+        }
+
+        [HttpGet("id")]
+        public Ogrenci Get(int id)
+        {
+            return ogrenciler.FirstOrDefault(o => o.Id == id);
+        }
+
+        [HttpPost]
+        public Ogrenci Post(Ogrenci ogrenci)
+        {
+            ogrenciler.Add(ogrenci);
+
+            return ogrenci;
+        }
+    }
+}
---
+++ Models/Ogrenci.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NETCoreWebAPIAttempt.Models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+    }
+}
